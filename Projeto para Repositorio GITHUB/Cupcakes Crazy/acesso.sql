fcadd08a7ce6a24e895d4b4e974b1e79f5ae86dc
Davidson Oliveira <74470344+dev-davidson@users.noreply.github.com>
2023-12-02 00:10:46 -0300

    Add files via upload

---
+++ Projeto para Repositorio GITHUB/Cupcakes Crazy/acesso.sql
@@ -0,0 +1,3 @@
+CREATE USER 'davidson'@'localhost' IDENTIFIED BY '03003762';
+GRANT ALL PRIVILEGES ON cupcake_crazy.* TO 'davidson'@'localhost';
+FLUSH PRIVILEGES;
---
+++ Projeto para Repositorio GITHUB/Cupcakes Crazy/processa_login.php
@@ -0,0 +1,2 @@
+// Conectar ao banco de dados
+$conn = new mysqli('localhost', 'davidson', '03003762', 'cupcake_crazy');
